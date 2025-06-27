89e4688540b638ee0ccb64856e152926f6c70b39
iconx <64724621+iconx2020a@users.noreply.github.com>
2025-06-27 19:52:27 +0200

    Update install-mongodb.sh

--- testapp/install-mongodb.sh
+++ testapp/install-mongodb.sh
@@ -3,16 +3,23 @@ sudo apt-get install gnupg curl
 curl -fsSL https://www.mongodb.org/static/pgp/server-8.0.asc | \
    sudo gpg -o /usr/share/keyrings/mongodb-server-8.0.gpg \
    --dearmor
- echo "deb [ signed-by=/usr/share/keyrings/mongodb-server-8.0.gpg ] http://repo.mongodb.org/apt/debian bookworm/mongodb-org/8.0 main" | sudo tee /etc/apt/sources.list.d/mongodb-org-8.0.list
- sudo apt-get install -y mongodb-org
- sudo systemctl start mongod
+ echo "deb [ signed-by=/usr/share/keyrings/mongodb-server-8.0.gpg ] http://repo.mongodb.org/apt/debian bookworm>
+ sudo apt update
 
-mongosh 
-show dbs
-#create db
-use admindb
-db.createCollection("users")
+ sudo apt-get install -y mongodb-org
+ sudo cp mongod.conf /etc/
+ sudo systemctl start mongod
+ sudo  systemctl enable mongod
+
+sleep 5
+
+DB_NAME="admindb"
+COLLECTION_NAME="users" 
+mongosh  <<EOF
+use $DB_NAME
+db.createCollection("$COLLECTION_NAME")
 show collections
-admindb>db.users.insert({email:"alice@medicoms.net", name: "Alice Cambo", password:"password123", role:"admin"}
-admindb>db.users.insert({email:"james@medicoms.net", name: "James Boyce", password:"password123", role:"member"}
-admindb> db.users.find()
+db.users.insert({email:"alice@medicoms.net", name: "Alice Cambo", password:"password123", role:"admin"}
+db.users.insert({email:"james@medicoms.net", name: "James Boyce", password:"password123", role:"member"}
+db.users.find()
+EOF
