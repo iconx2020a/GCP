5129f35cff5e48e39fd0d7069e7a12fa745fe7b3
iconx <64724621+iconx2020a@users.noreply.github.com>
2025-06-29 09:56:06 +0200

    Update Member.tsx

--- testapp/client/src/pages/Member.tsx
+++ testapp/client/src/pages/Member.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
-/*import {ReadMe} from "../pages/ReadMe";*/
 import Header from "../components/Header";
 const Nav = styled.nav`
   padding: 1em;
