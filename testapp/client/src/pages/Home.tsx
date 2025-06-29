a96d8441f095977fc6ab53d8b919a9a878edd8cd
iconx <64724621+iconx2020a@users.noreply.github.com>
2025-06-29 09:55:33 +0200

    Update Home.tsx

--- testapp/client/src/pages/Home.tsx
+++ testapp/client/src/pages/Home.tsx
@@ -1,9 +1,6 @@
-import React, { useState } from "react";
 import Header from "../components/Header";
 import Navigation from "../components/Navigation";
-
 export const Home = () => {
-
   return (
     <div>
       <Header />
