4f704275d1e1b204b9ef00f2222cd2e7d92e701a
iconx <64724621+iconx2020a@users.noreply.github.com>
2025-06-29 09:54:38 +0200

    Update install-mongodb.sh

--- testapp/install-mongodb.sh
+++ testapp/install-mongodb.sh
@@ -3,14 +3,16 @@ sudo apt-get install gnupg curl
 curl -fsSL https://www.mongodb.org/static/pgp/server-8.0.asc | \
    sudo gpg -o /usr/share/keyrings/mongodb-server-8.0.gpg \
    --dearmor
- echo "deb [ signed-by=/usr/share/keyrings/mongodb-server-8.0.gpg ] http://repo.mongodb.org/apt/debian bookworm>
- sudo apt update
+echo "deb [ signed-by=/usr/share/keyrings/mongodb-server-8.0.gpg ] \
+http://repo.mongodb.org/apt/debian bookworm/mongodb-org/8.0 main" | \
+sudo tee /etc/apt/sources.list.d/mongodb-org-8.0.list
+sudo apt update
 
- sudo apt-get install -y mongodb-org
- chmod 600 mongod.conf
- sudo cp mongod.conf /etc/
- sudo systemctl start mongod
- sudo  systemctl enable mongod
+sudo apt-get install -y mongodb-org
+chmod 600 mongod.conf
+sudo cp mongod.conf /etc/
+sudo systemctl start mongod
+sudo  systemctl enable mongod
 
 sleep 5
 
